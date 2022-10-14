ca6b803ba8b863b2c87d1f036459dd8091d14257
sanchitpasricha <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>
2022-10-14 19:15:23 +0530

    Touch ups

--- src/Message.jsx
+++ src/Message.jsx
@@ -7,14 +7,15 @@ const Message = forwardRef(({message, username}, ref) => {
   const isUser = username === message.username;
 
   return (
+    
     <div ref={ref} className={`message ${isUser && "message_user"}`}>
+      <p className='head'>{!isUser && `${message.username || 'Unknown User'}`} </p>
       <Card className={isUser ? "message_userCard" : "message_guestCard"}>
         <CardContent>
           <Typography
             variant='h5'
             component='h4'
           >
-            <p>{!isUser && `${message.username || 'Unknown User'}`} </p>
             <p className='body'>{message.message}</p>
           </Typography>
         </CardContent> 
